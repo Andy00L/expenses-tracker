7ae8f4702434d02825a9d5b370f6a48478f4c428
Andy <andy.luemba@protonmail.com>
2025-02-27 21:41:49 -0500

    test2

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
     <div>
       <h1 className="text-4xl font-bold">Expense Tracker</h1>
       <Link href="app/dashboard">dashboard</Link>
+      yes improvement
     </div>
   );
 }
