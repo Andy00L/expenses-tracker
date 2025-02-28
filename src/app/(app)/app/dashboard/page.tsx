ac1930472d4b5ba239739693e1bfe35a7e5ea610
Andy <andy.luemba@protonmail.com>
2025-02-27 21:50:30 -0500

    Addition

--- src/app/(app)/app/dashboard/page.tsx
+++ src/app/(app)/app/dashboard/page.tsx
@@ -1,7 +1,12 @@
+import Link from "next/link";
+
 export default function Page() {
   return (
     <div>
       <h1>Dashboard</h1>
+      <Link href="/app/account">Account</Link>
+      <br />
+      <Link href="/">main</Link>
     </div>
   );
 }
